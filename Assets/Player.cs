fdc64376375543f984183f5ffdc586324f173c30
Anis Mansour <mansour.anis@hotmail.com>
2020-05-23 17:13:18 -0700

    played moving on screen

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+     [Tooltip("In ms^-1")] [SerializeField] float xSpeed = 30f;
+    // speed variable  METERS PER SECONDS
+    [Tooltip("In m")] [SerializeField] float xRange = 20f;
     void Start()
     {
         
@@ -13,8 +16,21 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontalThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-        print(horizontalThrow);
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");//refer to everything to an horizontal axis
+        //throw if between 0 and 1 , goes up speed depends on the sensitivity and downs on the gravity
+        //
+        //print(xThrow);
+        float xOffset = xThrow * xSpeed * Time.deltaTime; // if click longer he will move further and faster
+                                                          //print(xOffset);
+
+
+        float newXPos = transform.localPosition.x + xOffset;
+        float clampedXpos = Mathf.Clamp(newXPos, -xRange, xRange); //position is between x range
+        //and give the value to the localPosition  
+        transform.localPosition = new Vector3(clampedXpos, transform.localPosition.y, transform.localPosition.z);
+
+
+
 
     }
 }
