7707143a89240526d9cecf7deac694837ac1f5ab
Anis Mansour <mansour.anis@hotmail.com>
2020-05-25 16:56:21 -0700

    bullets particles

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -7,16 +7,17 @@ public class Player : MonoBehaviour
 
      [Tooltip("In ms^-1")] [SerializeField] float xSpeed = 30f;
     // speed variable  METERS PER SECONDS
-    [Tooltip("In m")] [SerializeField] float xRange = 20f;
-    [Tooltip("In m")] [SerializeField] float yRange = 12f;
+    [Tooltip("In m")] [SerializeField] float xRange = 24f;
+    [Tooltip("In m")] [SerializeField] float yRange = 13f;
     [SerializeField] float RotationXFactor = -2f; //rotate the ship using factor *position
     //"each position multiplied by the factor will give the needed rotation
     [SerializeField] float controlFrontX = -5f; //factor that will controll the noise of the ship when moved up and down 
     [SerializeField] float RotationYFactor = 2f;
+    [SerializeField] float RotationZFactor = 1000f;
     [SerializeField] float controlFrontY = -20f;
 
 
-    float xThrow, yThrow; // moved them outside of movex and y () to be able to use them on rotation for moving noise of the ship
+    float xThrow, yThrow, zThrow; // moved them outside of movex and y () to be able to use them on rotation for moving noise of the ship
     void Start()
     {
         
@@ -25,11 +26,16 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        MoveX();
+       MoveX();
         MoveY();
+       //woopa();
         Rotation();
+        
+       
 
     }
+
+   
     private void Rotation()
     {
 
@@ -42,11 +48,6 @@ public class Player : MonoBehaviour
         float PosRotationY = transform.localPosition.x * RotationYFactor; ;
         float PosRotationZ = xThrow * controlFrontY;
         transform.localRotation = Quaternion.Euler(PosRotationX, PosRotationY, PosRotationZ);
-
-       
-
-
-
 
 
 
@@ -76,5 +77,20 @@ public class Player : MonoBehaviour
         float newYPos = transform.localPosition.y + yoffset;
         float clampedYpos = Mathf.Clamp(newYPos, -yRange, yRange);
         transform.localPosition = new Vector3(transform.localPosition.x, clampedYpos, transform.localPosition.z);
+    }
+
+    private void woopa()
+        //360 
+    {
+        { 
+        zThrow = CrossPlatformInputManager.GetAxis("Jump");
+        float zOffset = zThrow * RotationZFactor * Time.deltaTime;
+            print(zOffset);
+            transform.localRotation = Quaternion.Euler(0f, 0f, zOffset);
+
+
+        }
+
+
     }
 }
